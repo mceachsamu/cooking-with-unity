fb3be1dd51344b9921183111f5689f754f25d77d
sam mceachern <sammceachern@sams-MacBook-Pro.local>
2021-02-21 11:12:47 +1300

    updating some graphics settings

---
+++ Assets/script/c#/volumeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volumeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setShaderProperties();
+    }
+    private void setShaderProperties(){
+    this.GetComponent<Renderer>().material.SetVector("_Center", this.transform.position);
+    }
+}
