164973a401ff8ff72976bca7214d6948d430fe9b
sam mceachern <sammceachern@sams-MacBook-Pro.local>
2021-02-22 20:05:53 +1300

    adding smokepipe prefab

--- Assets/script/c#/smoke.cs
+++ Assets/script/c#/smoke.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ObjectFind;
 
 public class smoke : MonoBehaviour
 {
     //a counter that increments each update. used to move smoke effect with time
     private int count = 0;
 
-    public GameObject water;
+    private GameObject potController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //initialize water
+        potController = FindFirstWithTag("GameController");
     }
 
     // Update is called once per frame
@@ -20,7 +22,7 @@ public class smoke : MonoBehaviour
     {
         //move upwards with the water level
         Vector3 pos = this.transform.position;
-        pos.y = water.transform.position.y;
+        pos.y = potController.transform.position.y;
         this.transform.position = pos;
 
         count++;
@@ -29,6 +31,6 @@ public class smoke : MonoBehaviour
 
     private void SetShaderProperties(){
         this.GetComponent<Renderer>().material.SetInt("_Count", count);
-        this.GetComponent<Renderer>().material.SetVector("_Color", water.GetComponent<potController>().GetColor());
+        this.GetComponent<Renderer>().material.SetVector("_Color", potController.GetComponent<potController>().GetColor());
     }
 }
