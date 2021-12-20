ed31682edc7cd0f33c66b856e60b7a22aef6ec19
mceachsamu <samisacamel@outlook.com>
2021-12-20 22:46:43 +1300

    cleaning up camera script var names

--- Assets/scripts/c#/controllers/CameraController.cs
+++ Assets/scripts/c#/controllers/CameraController.cs
@@ -4,76 +4,74 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //positions holds tell the controller the different positions it can move to
-    //each object in this array should have a CameraTag script attached
-    public GameObject[] positions;
+    // Positions holds tell the controller the different positions it can move to
+    // Each object in this array should have a CameraTag script attached
+    public GameObject[] Positions;
 
-    //the animation speed of the camera
-    public AnimationCurve cameraMoveSpeed;
+    // The animation speed of the camera
+    public AnimationCurve CameraMoveSpeed;
 
-    //the number of seconds it takes to transition through animation
+    // The number of seconds it takes to transition through animation
     public float AnimationTimeInSeconds = 3;
 
-    //the position the camera is starting at
-    private Vector3 startPosition;
+    // The position the camera is starting at
+    private Vector3 _startPosition;
 
-    //the position the camera is moving to/ is currently at
-    private Vector3 targetPosition;
+    // The position the camera is moving to/ is currently at
+    private Vector3 _targetPosition;
 
+    // The rotation the camera is starting at
+    private Quaternion _startRotation;
 
-    //the rotation the camera is starting at
-    private Quaternion startRotation;
+    // The rotation the camera is moving to/ is currently at
+    private Quaternion _targetRotation;
 
-    //the rotation the camera is moving to/ is currently at
-    private Quaternion targetRotation;
-
-
-    //the time the current animation started at
-    private float startTime;
+    // The time the current animation started at
+    private float _startTime;
 
     void Start()
     {
-        if (positions.Length == 0)
+        if (Positions.Length == 0)
         {
             print("need to have atleast one position object on camera");
         }
 
         // Initialize target position to the first position in the list
-        startPosition = positions[0].transform.position;
-        targetPosition = positions[0].transform.position;
+        _startPosition = Positions[0].transform.position;
+        _targetPosition = Positions[0].transform.position;
         
         // Initialize target rotation to the first rotation in the list
-        startRotation = positions[0].transform.rotation;
-        targetRotation = positions[0].transform.rotation;
+        _startRotation = Positions[0].transform.rotation;
+        _targetRotation = Positions[0].transform.rotation;
     }
 
     void Update()
     {
-        //listen for new position requests and update targetPosition and startTime
+        // Listen for new position requests and update targetPosition and startTime
         ListenToInput();
 
-        //get the speed the camera should currently be moving at (if its changing positions)
-        float curFrame = GetAnimationFrame(cameraMoveSpeed, startTime);
+        // Get the speed the camera should currently be moving at (if its changing positions)
+        float curFrame = GetAnimationFrame(CameraMoveSpeed, _startTime);
 
-        //interpolate between its current position and its new position
-        this.transform.position = TranslateToPosition(startPosition, targetPosition, curFrame);
-        this.transform.rotation = TranslateToRotation(startRotation, targetRotation, curFrame);
+        // Interpolate between its current position and its new position
+        this.transform.position = TranslateToPosition(_startPosition, _targetPosition, curFrame);
+        this.transform.rotation = TranslateToRotation(_startRotation, _targetRotation, curFrame);
     }
 
     private void ListenToInput()
     {
-        for (int i = 0; i < positions.Length; i++) {
-            if (Input.GetKeyDown(positions[i].GetComponent<CameraPosition>().KeyStroke))
+        for (int i = 0; i < Positions.Length; i++) {
+            if (Input.GetKeyDown(Positions[i].GetComponent<CameraPosition>().KeyStroke))
             {
-                if (positions[i] != null)
+                if (Positions[i] != null)
                 {
-                    startTime = Time.time;
+                    _startTime = Time.time;
 
-                    startPosition = this.transform.position;
-                    startRotation = this.transform.rotation;
+                    _startPosition = this.transform.position;
+                    _startRotation = this.transform.rotation;
 
-                    targetRotation = positions[i].transform.rotation;
-                    targetPosition = positions[i].transform.position;
+                    _targetRotation = Positions[i].transform.rotation;
+                    _targetPosition = Positions[i].transform.position;
                 }
             }
         }
@@ -85,7 +83,7 @@ public class CameraController : MonoBehaviour
         float endTime = AnimationTimeInSeconds;
         float normalizedTime = timeSinceStart/endTime;
 
-        //using normalized time will make the function go from 0 to 1 in AnimationTimeInSeconds;
+        // Using normalized time will make the function go from 0 to 1 in AnimationTimeInSeconds;
         float speed = speedCurve.Evaluate(normalizedTime);
         return speed;
     }
